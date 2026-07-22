00f415c458fe49f4dabcde3c99fa6dc8c421135e
Melisa Aytaç <92992545+mvllisa@users.noreply.github.com>
2026-07-22 23:18:28 +0300

    Delete is-divisible-to-them.js

--- is-divisible-to-them.js
+++
@@ -1,6 +0,0 @@
-function isDivisible(n, x, y) {
-  // Check if a number n is divisible by two numbers x and y.
-  // All inputs are positive, non-zero numbers.
-  const bigNumber = x*y;
-  return n % bigNumber === 0;
-}
