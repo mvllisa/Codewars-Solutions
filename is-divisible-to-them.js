80190c1b910eb99100b0efe0157ed1fa803386fe
Melisa Aytaç <mellisa.aytacc@gmail.com>
2026-07-22 23:20:43 +0300

    solution for divisible numbers kata

--- is-divisible-to-them.js
+++ is-divisible-to-them.js
@@ -1,6 +1,5 @@
 function isDivisible(n, x, y) {
   // Check if a number n is divisible by two numbers x and y.
   // All inputs are positive, non-zero numbers.
-  const bigNumber = x*y;
-  return n % bigNumber === 0;
+  return n % x === 0 && n % y === 0;
 }
