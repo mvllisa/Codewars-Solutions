26b2999e863c2bcf8f06efc0589a0de90fd9c4de
Melisa Aytaç <mellisa.aytacc@gmail.com>
2026-07-22 23:06:54 +0300

    solution for secret love message kata

---
+++ love-message.js
@@ -0,0 +1,8 @@
+function greet(name){
+  // Melisa has written a function that returns a greeting for a user.
+  // However, she's in love with Ozenc, and would like to greet him slightly different. 
+  
+  if (name !== "Ozenc") return "Hello, " + name + "!";
+  return "Hello, my love!";
+    
+}
